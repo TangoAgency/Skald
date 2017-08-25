48c49e54fff75fa11f626269e56984c0dabe728d
Marcin Habzda <marcin.habzda1@gmail.com>
2017-08-25 15:08:43 +0200

    Extend models

--- core/src/main/java/agency/tango/core/models/SkaldPlaylist.java
+++ core/src/main/java/agency/tango/core/models/SkaldPlaylist.java
@@ -1,4 +1,15 @@
 package agency.tango.core.models;
 
 public class SkaldPlaylist {
+  private String uri;
+
+  private String name;
+
+  public String getUri() {
+    return uri;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
--- core/src/main/java/agency/tango/core/models/SkaldTrack.java
+++ core/src/main/java/agency/tango/core/models/SkaldTrack.java
@@ -4,6 +4,11 @@ public class SkaldTrack {
   private String uri;
   private String name;
 
+  public SkaldTrack(String uri, String name) {
+    this.uri = uri;
+    this.name = name;
+  }
+
   public String getUri() {
     return uri;
   }
