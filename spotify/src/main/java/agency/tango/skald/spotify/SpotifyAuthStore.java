4a9e38a4ae07da7879cbcda85d9a776fc0c81009
Marcin Habzda <marcin.habzda1@gmail.com>
2017-09-04 16:07:58 +0200

    Fix storing data in SharedPreferences to be json to simplify implementation

--- spotify/src/main/java/agency/tango/skald/spotify/SpotifyAuthStore.java
+++ spotify/src/main/java/agency/tango/skald/spotify/SpotifyAuthStore.java
@@ -3,12 +3,14 @@ package agency.tango.skald.spotify;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+
 import agency.tango.skald.core.SkaldAuthData;
 import agency.tango.skald.core.SkaldAuthStore;
 
 public class SpotifyAuthStore implements SkaldAuthStore {
-  private static final String AUTH_TOKEN_KEY = "accessToken";
-  private static final String EXPIRES_IN_KEY = "expiresIn";
+  private static final String SPOTIFY_JSON_KEY = "spotify_json";
+  private static final String SPOTIFY_FILE_KEY = "agency.tango.skald.spotify.SPOTIFY_FILE_KEY";
 
   @Override
   public void save(SkaldAuthData skaldAuthData, Context context) {
@@ -17,29 +19,28 @@ public class SpotifyAuthStore implements SkaldAuthStore {
     SpotifyAuthData spotifyAuthorizationData =
         (SpotifyAuthData) skaldAuthData;
 
+    Gson gson = new Gson();
+    String json = gson.toJson(spotifyAuthorizationData);
+
     SharedPreferences.Editor editor = sharedPreferences.edit();
-    editor.putString(AUTH_TOKEN_KEY, spotifyAuthorizationData.getOauthToken());
-    editor.putInt(EXPIRES_IN_KEY, spotifyAuthorizationData.getExpiresIn());
-    editor.commit();
+    editor.putString(SPOTIFY_JSON_KEY, json);
+    editor.apply();
   }
 
   @Override
   public SkaldAuthData restore(Context context) {
     SharedPreferences sharedPreferences = getSharedPreferences(context);
 
-    String authToken = sharedPreferences.getString(AUTH_TOKEN_KEY, "");
-    int expiresIn = sharedPreferences.getInt(EXPIRES_IN_KEY, 0);
-
-    if(authToken.equals("") && expiresIn == 0) {
+    String json = sharedPreferences.getString(SPOTIFY_JSON_KEY, "");
+    if(json.equals("")) {
       return null;
     }
-    else {
-      return new SpotifyAuthData(authToken, expiresIn);
-    }
+
+    Gson gson = new Gson();
+    return gson.fromJson(json, SpotifyAuthData.class);
   }
 
   private SharedPreferences getSharedPreferences(Context context) {
-    return context.getSharedPreferences(context.getString(R.string.preference_file_key),
-        Context.MODE_PRIVATE);
+    return context.getSharedPreferences(SPOTIFY_FILE_KEY, Context.MODE_PRIVATE);
   }
 }
