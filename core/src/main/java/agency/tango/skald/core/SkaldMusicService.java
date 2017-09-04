523d0cafdee4893173f05ef5f4ac1fbc0001c961
Marcin Habzda <marcin.habzda1@gmail.com>
2017-09-04 16:15:01 +0200

    Extend SkaldMusicService class *Add authErrorListners *Add saving of SkaldAuthData *Implement prepare method *Add get methods getting proper implementations from factories

--- core/src/main/java/agency/tango/skald/core/SkaldMusicService.java
+++ core/src/main/java/agency/tango/skald/core/SkaldMusicService.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,19 +20,20 @@ public class SkaldMusicService {
 
   private final List<OnPreparedListener> onPreparedListeners = new ArrayList<>();
   private final List<OnErrorListener> onErrorListeners = new ArrayList<>();
+  private final List<AuthErrorListener> authErrorListeners = new ArrayList<>();
   private final List<Provider> providers = new ArrayList<>();
   private final Context context;
-  //for now for simplicity only one player
+
   private Player player;
   private SkaldTrack currentTrack;
-  private UriParser uriParser;
+  private SkaldAuthData skaldAuthData;
 
-
-  private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+  private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
     @Override
     public void onReceive(Context context, Intent intent) {
-      String message = intent.getStringExtra("token");
-      Log.d(TAG, "Got message: " + message);
+      skaldAuthData = intent.getExtras().getParcelable("authData");
+      getSkaldAuthStore().save(skaldAuthData, context);
+      player = getPlayer();
     }
   };
 
@@ -42,37 +42,26 @@ public class SkaldMusicService {
     this.context = context.getApplicationContext();
     LocalBroadcastManager
         .getInstance(context.getApplicationContext())
-        .registerReceiver(mMessageReceiver, new IntentFilter(INTENT_ACTION));
-
-    //for now assume existing of only one provider
-    uriParser = this.providers.get(0).getParser();
+        .registerReceiver(messageReceiver, new IntentFilter(INTENT_ACTION));
   }
 
   public void setSource(SkaldTrack skaldTrack) {
-    this.currentTrack = uriParser.parseSkaldTrack(skaldTrack);
-
-    if(player == null) {
-      player = SkaldMusicService.this.providers.get(0)
-          .getPlayerFactory()
-          .getPlayerFor(currentTrack);
-
-      player.addPlayerReadyListener(new OnPlayerReadyListener() {
-        @Override
-        public void onPlayerReady(Player player) {
-          for(OnPreparedListener onPreparedListener: onPreparedListeners){
-            Log.d(TAG, "onPlayerReady");
-            onPreparedListener.onPrepared(SkaldMusicService.this);
-          }
-        }
-      });
-    }
+    this.currentTrack = skaldTrack;
   }
 
   public void setSource(SkaldPlaylist skaldPlaylist) {
   }
 
   public void prepare() {
-
+    skaldAuthData = getSkaldAuthStore().restore(context);
+    if(skaldAuthData == null) {
+      for(AuthErrorListener authErrorListener : authErrorListeners) {
+        authErrorListener.onAuthError(getAuthError());
+      }
+    }
+    else {
+      player = getPlayer();
+    }
   }
 
   public void prepareAsync() {
@@ -114,6 +103,14 @@ public class SkaldMusicService {
     onPreparedListeners.remove(onPreparedListener);
   }
 
+  public void addAuthErrorListener(AuthErrorListener authErrorListener) {
+    authErrorListeners.add(authErrorListener);
+  }
+
+  public void removeAuthErrorListener(AuthErrorListener authErrorListener) {
+    authErrorListeners.remove(authErrorListener);
+  }
+
   public List<SkaldTrack> searchTrack(String query) {
     return Collections.emptyList();
   }
@@ -122,11 +119,33 @@ public class SkaldMusicService {
     return Collections.emptyList();
   }
 
-  //private SkaldTrack parseSkaldTrack(SkaldTrack skaldTrack) {
-  //  Uri uri = skaldTrack.getUri();
-  //  String authority = uri.getAuthority();
-  //  if(authority.equals("spotify")) {
-  //    return new SpotifyTrack(
-  //
-  //}
+  private Player getPlayer() {
+    //for now assume existing of only one provider
+    Player player = providers.get(0)
+        .getPlayerFactory()
+        .getPlayerFor(currentTrack, skaldAuthData);
+
+    player.addPlayerReadyListener(new OnPlayerReadyListener() {
+      @Override
+      public void onPlayerReady(Player player) {
+        for(OnPreparedListener onPreparedListener: onPreparedListeners){
+          onPreparedListener.onPrepared(SkaldMusicService.this);
+        }
+      }
+    });
+
+    return player;
+  }
+
+  private SkaldAuthStore getSkaldAuthStore() {
+    return providers.get(0)
+        .getSkaldAuthStoreFactory()
+        .getSkaldAuthStore();
+  }
+
+  private AuthError getAuthError() {
+    return providers.get(0)
+        .getAuthErrorFactory()
+        .getAuthError();
+  }
 }
