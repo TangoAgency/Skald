59d9c33d9100b5f36684b77bf3504a9b610927e5
Marcin Habzda <marcin.habzda1@gmail.com>
2017-09-04 12:48:17 +0200

    Fix SkaldAuthData to implements Parcelable to be able to pass it to Intent *Implement SpotifyAuthData

---
+++ core/src/main/java/agency/tango/skald/core/SkaldAuthData.java
@@ -0,0 +1,6 @@
+package agency.tango.skald.core;
+
+import android.os.Parcelable;
+
+public abstract class SkaldAuthData implements Parcelable {
+}
---
+++ spotify/src/main/java/agency/tango/skald/spotify/SpotifyAuthData.java
@@ -0,0 +1,53 @@
+package agency.tango.skald.spotify;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import agency.tango.skald.core.SkaldAuthData;
+
+public class SpotifyAuthData extends SkaldAuthData {
+  static final Parcelable.Creator<SpotifyAuthData> CREATOR =
+      new Parcelable.Creator<SpotifyAuthData>() {
+    @Override
+    public SpotifyAuthData createFromParcel(Parcel in) {
+      return new SpotifyAuthData(in);
+    }
+
+    @Override
+    public SpotifyAuthData[] newArray(int size) {
+      return new SpotifyAuthData[size];
+    }
+  };
+
+  private final String oauthToken;
+  private final int expiresIn;
+
+  public SpotifyAuthData(String oauthToken, int expiresIn) {
+    this.oauthToken = oauthToken;
+    this.expiresIn = expiresIn;
+  }
+
+  private SpotifyAuthData(Parcel in) {
+    oauthToken = in.readString();
+    expiresIn = in.readInt();
+  }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeString(oauthToken);
+    dest.writeInt(expiresIn);
+  }
+
+  public String getOauthToken() {
+    return oauthToken;
+  }
+
+  public int getExpiresIn() {
+    return expiresIn;
+  }
+}
