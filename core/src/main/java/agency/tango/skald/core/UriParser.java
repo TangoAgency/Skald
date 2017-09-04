09b11aa42de1bb79b782e9235337f11e40f1efc4
Marcin Habzda <marcin.habzda1@gmail.com>
2017-09-04 12:38:10 +0200

    Delete UriParser

--- core/src/main/java/agency/tango/skald/core/UriParser.java
+++
@@ -1,7 +0,0 @@
-package agency.tango.skald.core;
-
-import agency.tango.skald.core.models.SkaldTrack;
-
-public abstract class UriParser {
-  public abstract SkaldTrack parseSkaldTrack(SkaldTrack skaldTrack);
-}
