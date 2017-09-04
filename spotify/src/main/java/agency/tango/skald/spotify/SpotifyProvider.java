de78a3cbde68f85ec2a6ee42e7bd087713f5cc87
Marcin Habzda <marcin.habzda1@gmail.com>
2017-09-04 16:01:57 +0200

    Add Factories and getFactory methods which returns them to SpotifyProvider

--- spotify/src/main/java/agency/tango/skald/spotify/SpotifyProvider.java
+++ spotify/src/main/java/agency/tango/skald/spotify/SpotifyProvider.java
@@ -2,18 +2,18 @@ package agency.tango.skald.spotify;
 
 import android.content.Context;
 
+import agency.tango.skald.core.AuthError;
+import agency.tango.skald.core.AuthErrorFactory;
 import agency.tango.skald.core.Player;
 import agency.tango.skald.core.PlayerFactory;
 import agency.tango.skald.core.Provider;
 import agency.tango.skald.core.SkaldAuthData;
+import agency.tango.skald.core.SkaldAuthStore;
+import agency.tango.skald.core.SkaldAuthStoreFactory;
 import agency.tango.skald.core.models.SkaldTrack;
 import agency.tango.skald.spotify.models.SpotifyTrack;
 
 public class SpotifyProvider extends Provider {
-
-  public static final String SPOTIFY_CLIENT_ID = "8c43f75741454312adbbbb9d5ac6cb5b";
-  public static final String SPOTIFY_REDIRECT_URI = "spotify-example-marcin-first-app://callback";
-
   public static final String EXTRA_CLIENT_ID = "SPOTIFY_CLIENT_ID";
   public static final String EXTRA_REDIRECT_URI = "SPOTIFY_REDIRECT_URI";
 
@@ -38,6 +38,17 @@ public class SpotifyProvider extends Provider {
     return new SpotifyPlayerFactory(context, clientId);
   }
 
+  @Override
+  public SkaldAuthStoreFactory getSkaldAuthStoreFactory() {
+    return new SpotifyAuthStoreFactory();
+  }
+
+  @Override
+  public AuthErrorFactory getAuthErrorFactory() {
+    return new SpotifyAuthErrorFactory(context,
+        clientId, redirectUri);
+  }
+
   public String getClientId() {
     return clientId;
   }
@@ -47,7 +58,6 @@ public class SpotifyProvider extends Provider {
   }
 
   public static class SpotifyPlayerFactory extends PlayerFactory {
-
     private final Context context;
     private String clientId;
 
@@ -63,6 +73,30 @@ public class SpotifyProvider extends Provider {
         return new SkaldSpotifyPlayer(context, clientId, accessToken);
       }
       return null;
+    }
+  }
+
+  private static class SpotifyAuthStoreFactory extends SkaldAuthStoreFactory {
+    @Override
+    public SkaldAuthStore getSkaldAuthStore() {
+      return new SpotifyAuthStore();
+    }
+  }
+
+  private static class SpotifyAuthErrorFactory extends AuthErrorFactory {
+    private final Context context;
+    private final String clientId;
+    private final String redirectUri;
+
+    private SpotifyAuthErrorFactory(Context context, String clientId, String redirectUri) {
+      this.context = context;
+      this.clientId = clientId;
+      this.redirectUri = redirectUri;
+    }
+
+    @Override
+    public AuthError getAuthError() {
+      return new SpotifyAuthError(context, clientId, redirectUri);
     }
   }
 }
