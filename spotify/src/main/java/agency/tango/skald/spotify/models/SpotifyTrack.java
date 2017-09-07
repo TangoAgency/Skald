0bceb32634bf7dd9b2c211ea11e86004f8983de1
Marcin Habzda <marcin.habzda1@gmail.com>
2017-09-07 11:49:37 +0200

    Add new constructor to SpotifyTrack

--- spotify/src/main/java/agency/tango/skald/spotify/models/SpotifyTrack.java
+++ spotify/src/main/java/agency/tango/skald/spotify/models/SpotifyTrack.java
@@ -3,14 +3,15 @@ package agency.tango.skald.spotify.models;
 import android.net.Uri;
 
 import agency.tango.skald.core.models.SkaldTrack;
+import agency.tango.skald.spotify.api.models.Track;
 
 public class SpotifyTrack extends SkaldTrack {
 
-  //SpotifyTrack(Track track) {
-  //  this(track.getUri());
-  //}
-
   public SpotifyTrack(Uri uri) {
     super(uri);
+  }
+
+  public SpotifyTrack(Track track) {
+    this(Uri.parse(String.format("skald://spotify/track/%s", track.getUri())));
   }
 }
