5ceb117249d6161fd040c1e2ead0876faeef0d67
Marcin Habzda <marcin.habzda1@gmail.com>
2017-09-07 14:05:39 +0200

    Add SuppressLint annotation as we dont focus on Deezer right now

--- deezer/src/main/java/agency/tango/skald/deezer/DeezerAuthData.java
+++ deezer/src/main/java/agency/tango/skald/deezer/DeezerAuthData.java
@@ -1,11 +1,13 @@
 package agency.tango.skald.deezer;
 
+import android.annotation.SuppressLint;
 import android.os.Parcel;
 
 import com.deezer.sdk.network.connect.DeezerConnect;
 
 import agency.tango.skald.core.SkaldAuthData;
 
+@SuppressLint("ParcelCreator")
 public class DeezerAuthData extends SkaldAuthData {
   private final DeezerConnect deezerConnect;
 
